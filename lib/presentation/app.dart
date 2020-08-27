96676520945c61ca2fc3bb3772181bec433f4e0a
Bhushan <bhushan.akerfoods@gmail.com>
2020-08-27 18:41:51 +0530

    Resolve problem of Theme loading with dimension utils initialisation

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,59 +1,111 @@
 import 'package:flutter/material.dart';
 
+import '../common/utils/pixel_dimension_util.dart';
+import 'login/enter_otp_screen.dart';
+import 'login/enter_phone_number_screen.dart';
 import 'theme/app_themes.dart';
 
 class App extends StatelessWidget {
+  final GlobalKey<NavigatorState> _navigatorStateKey =
+      GlobalKey<NavigatorState>();
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        builder: (context, widget) {
+          PixelDimensionUtil.init(context, allowFontScaling: true);
+          return _wrapWithThemedMaterialApp();
+        },
+      );
+
+  MaterialApp _wrapWithThemedMaterialApp() => MaterialApp(
+        navigatorKey: _navigatorStateKey,
+        builder: (context, widget) {
+          PixelDimensionUtil.init(
+            context,
+            allowFontScaling: true,
+            navigator: _navigatorStateKey,
+          );
+          return widget;
+        },
+        title: 'Aker Foods Retail',
+        theme: AppTheme.defaultTheme(),
+        routes: {
+          '/': (context) => EnterPhoneNumberScreen(
+                title: 'Login',
+              ),
+          '/otp': (context) => EnterOtpScreen(),
+        },
+        initialRoute: '/',
+      );
+}
+
+/*class App extends StatelessWidget {
+  final GlobalKey<NavigatorState> _navigatorStateKey =
+      GlobalKey<NavigatorState>();
+
   @override
   Widget build(BuildContext context) {
+    PixelDimensionUtil.init(
+      context,
+      allowFontScaling: true,
+      navigator: _navigatorStateKey,
+    );
+    return AppThemeUpdater(
+      child: Builder(
+        builder: (context) {
+          PixelDimensionUtil.init(
+            context,
+            allowFontScaling: true,
+            navigator: _navigatorStateKey,
+          );
+          return _materialApp();
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBuilder(
+      builder: (context) {
+        PixelDimensionUtil.init(
+          context,
+          allowFontScaling: true,
+          navigator: _navigatorStateKey,
+        );
+        final abSp = 35.sp;
+        print('=================== abSp: $abSp');
+        return _materialApp();
+      },
+    );
+  }
+
+  MaterialApp _materialApp() {
+    print('=================== Building Material App');
     return MaterialApp(
+      navigatorKey: _navigatorStateKey,
+      builder: (context, widget) {
+        PixelDimensionUtil.init(
+          context,
+          allowFontScaling: true,
+          navigator: _navigatorStateKey,
+        );
+
+        final h1 = Theme.of(context).textTheme.headline1.fontSize;
+        final ah1 = Theme.of(context).textTheme.appHeadline1.fontSize;
+        print('=================== Headline1: $h1');
+        print('=================== App Headline1: $ah1');
+        return widget;
+      },
       title: 'Aker Foods Retail',
-      theme: AppTheme.defaultTheme(context),
-      home: DashboardScreen(title: 'Dashboard'),
-    );
-  }
-}
-
-class DashboardScreen extends StatefulWidget {
-  DashboardScreen({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _DashboardScreenState createState() => _DashboardScreenState();
-}
-
-class _DashboardScreenState extends State<DashboardScreen> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have following number of items in cart:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+      theme: AppTheme.defaultTheme(),
+      routes: {
+        '/': (context) => EnterPhoneNumberScreen(
+              title: 'Login',
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Cart',
-        child: Icon(Icons.add_shopping_cart),
-      ),
+        '/otp': (context) => EnterOTPScreen(),
+      },
+      initialRoute: '/',
     );
   }
-}
+}*/
