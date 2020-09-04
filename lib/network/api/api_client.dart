ec990764f029fa0b5ca610e9acd8d68cc5d4d67a
Bhushan <bhushan.akerfoods@gmail.com>
2020-09-05 02:51:41 +0530

    Configure the firebase token as authorization header in ApiClient

--- lib/network/api/api_client.dart
+++ lib/network/api/api_client.dart
@@ -1,27 +1,29 @@
 import 'package:aker_foods_retail/config/configuration.dart';
 import 'package:aker_foods_retail/network/http/http_constants.dart';
+import 'package:flutter/foundation.dart';
 
 import '../http/http_client.dart';
+import 'api_header_constants.dart';
 
 class ApiClient extends HttpClient {
   ApiClient()
       : super(
           host: Configuration.host,
-          header: getAuthenticationHeader(),
+          header: getAuthorizationHeader(),
         );
 
-  static Map<String, String> getAuthenticationHeader() {
-    return {
-      HttpConstants.authorization: Configuration.token,
-      HttpConstants.contentType: HttpConstants.jsonContentType,
+  static Map<String, String> getAuthorizationHeader() => {
+        HttpConstants.contentType: HttpConstants.jsonContentType,
+        HttpConstants.authorization: '$firebaseTokenPrefix NO_TOKEN',
+      };
+
+  void updateAuthorizationHeader(String firebaseToken) {
+    debugPrint('===== Updating AuthorizationHeader =====');
+    header = {
+      ...header,
+      HttpConstants.authorization: '$firebaseTokenPrefix $firebaseToken',
     };
   }
 
-  void updateAuthenticationHeader(String accessToken) {
-    header = {...header, 'Authorization': 'Bearer $accessToken'};
-  }
-
-  Map<String, String> getClientAuthenticationHeader() {
-    return header;
-  }
+  Map<String, String> getClientAuthenticationHeader() => header;
 }
