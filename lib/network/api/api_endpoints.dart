3a7115137ca9899b5537b8fdb64515c9e274a68b
Bhushan <bhushan.akerfoods@gmail.com>
2020-09-14 21:56:36 +0530

    Fix base url & endpoints AND Remove unnecessary files

--- lib/network/api/api_endpoints.dart
+++ lib/network/api/api_endpoints.dart
@@ -1,5 +1,5 @@
 class ApiEndpoints {
-  static const String userProfile = '/api/v1/user_profiles/';
-  static const String newAddress = '/api/v1/addresses/';
-  static const String societies = '/api/v1/societies/';
+  static const String userProfile = 'api/v1/user_profiles/';
+  static const String newAddress = 'api/v1/addresses/';
+  static const String societies = 'api/v1/societies/';
 }
