4afd69c52cadb9a7ed5962d63b80d4f09485c82a
Bhushan <bhushan.akerfoods@gmail.com>
2020-10-09 01:56:59 +0530

    Implement product's subcategory parsing inside product's category entity

--- lib/domain/entities/product_category_entity.dart
+++ lib/domain/entities/product_category_entity.dart
@@ -1,13 +1,17 @@
+import 'package:aker_foods_retail/domain/entities/product_subcategory_entity.dart';
+
 class ProductCategoryEntity {
   final int id;
   final String name;
   final String description;
   final String imageUrl;
+  final List<ProductSubcategoryEntity> subcategories;
 
   ProductCategoryEntity({
     this.id,
     this.name,
     this.description,
     this.imageUrl,
+    this.subcategories,
   });
 }
