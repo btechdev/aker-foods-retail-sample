e7a953376717e30c0bbd77b4847cd4e61f5c0876
Bhushan <bhushan.akerfoods@gmail.com>
2020-10-14 02:51:19 +0530

    Configure OneSignal app id from build config for both dev & prod environments

--- lib/config/configuration.dart
+++ lib/config/configuration.dart
@@ -2,26 +2,29 @@ import 'package:aker_foods_retail/config/environment_enum.dart';
 import 'package:flutter/foundation.dart';
 
 class Configuration {
-  static String _razorpayKey;
-
-  static String _host;
   static String _environment;
+  static String _host;
+  static String _razorpayKey;
+  static String _oneSignalAppId;
   static bool _shouldEnableAllLogs;
   static bool _shouldEnableCrashlytics;
 
   void setConfigurationValues(Map<String, dynamic> value) {
+    _environment = value['environment'] ?? describeEnum(Environment.dev);
     _host = value['host'];
     _razorpayKey = value['razorpayKey'];
-    _environment = value['environment'] ?? describeEnum(Environment.dev);
+    _oneSignalAppId = value['oneSignalAppId'];
     _shouldEnableAllLogs = value['shouldEnableAllLogs'] ?? true;
     _shouldEnableCrashlytics = value['shouldEnableCrashlytics'] ?? true;
   }
+
+  static String get environment => _environment;
 
   static String get host => _host;
 
   static String get razorpayKey => _razorpayKey;
 
-  static String get environment => _environment;
+  static String get oneSignalAppId => _oneSignalAppId;
 
   static bool get shouldEnableAllLogs => _shouldEnableAllLogs;
 
